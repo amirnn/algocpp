6500564a3032c27ef3e150e5dadb8b5c8b5879fc
Amir Nourinia <amir.nourinia@tum.de>
2022-07-21 18:51:15 +0200

    tests fixed

--- tests/week1/test1.cc
+++ tests/week1/test1.cc
@@ -1,6 +1,5 @@
-#include "doctest/doctest.h"
-
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest/doctest.h"
 
 int factorial(int number) { return number <= 1 ? number : factorial(number - 1) * number; }
 
