452a3afb5ea5f3765918b13cde0eff7de573e968
Amir Nourinia <amir.nourinia@tum.de>
2022-07-22 10:11:47 +0200

    changed structure

---
+++ src/princeton/libraries/union_find/union_find.cc
@@ -0,0 +1,25 @@
+#include "union_find.hpp"
+
+
+namespace union_find {
+
+
+/**
+ * @brief Construct a new UnionFind::UnionFind object
+ *
+ * @param n number of nodes
+ * @param id
+ */
+    UnionFind::UnionFind(u64 node_count) : m_node_count{node_count} {
+
+    }
+
+/**
+ * @brief Destroy the UnionFind::UnionFind object
+ *
+ */
+    UnionFind::~UnionFind() {
+
+    }
+
+}
